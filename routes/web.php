ce1bd4b53a2fa18bc1ab4974a18257279b8b2d1d
Manuel <manuel.devito@icloud.com>
2020-12-21 20:25:04 +0100

    Esercizio

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/primo-passo', function () {
+    $data = [
+        'nome' => 'Manuel'
+    ];
+    return view('primo', $data);
 });
